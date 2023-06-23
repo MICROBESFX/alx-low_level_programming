994735eb7cc8da468804390d23cc8fcc14dc470f
MICROBESFX <megbuzie.victory@gmail.com>
2023-06-22 18:57:47 -0700

    fhfjf

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,22 @@
 #include "main.h"
+
 /**
-* main - Test function for positive or negative
-* Return: 0
-*/
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
 int main(void)
 {
-int i;
-i = 0;
-positive_or_negative(i);
-return (0);
+    int i;
+
+    i = 0;
+    positive_or_negative(i);
+
+    return (0);
+}
+
+void positive_or_negative(int i)
+{
+    // Your implementation of positive_or_negative function goes here
+    // This is just a placeholder function declaration
 }
 

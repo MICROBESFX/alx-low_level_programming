ea2c4c229acf66723150c8a48b9f1b33271ef247
MICROBESFX <megbuzie.victory@gmail.com>
2023-06-27 18:17:09 -0700

    ghbgd

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * puts2 - function should print only one character out of two
+* puts2 - function should print only one character out of two
  * starting with the first one
  * @str: input
- * Return: print
- */
+* Return: print
+*/
 void puts2(char *str)
 {
 int longi = 0;
